a87832422793d92a4694ef97b6f9188504bd1f30
emmalod <emma.lod@gmail.com>
2023-04-24 15:44:54 +0200

    GitHub_pages

---
+++ Frontend/.nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from 'C:/Users/emmal/Documents/GitHub/VenTour/Frontend/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
---
+++ Frontend/.nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/emmal/Documents/GitHub/VenTour/Frontend/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
