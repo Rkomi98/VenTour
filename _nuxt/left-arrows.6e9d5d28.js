c04d3020c15adcbc846175c6325fc1faf86057ba
Mirko Calcaterra <mirko-98@libero.it>
2023-07-10 12:16:43 +0200

    Updates

--- _nuxt/left-arrows.6e9d5d28.js
+++
@@ -1,1 +0,0 @@
-import"./entry.5a643f78.js";const t=""+new URL("left-arrows.17681d4e.png",import.meta.url).href;export{t as _};
--- server/_nuxt/_id_-styles-2.mjs-e95a85bb.js
+++
@@ -1,5 +0,0 @@
-const Supervisor_vue_vue_type_style_index_0_lang = ".link,.subtitle{text-decoration:none}.hover-content2{color:#000;left:50%;position:relative;text-align:center;text-decoration:none;top:50%;transform:translate(-50%,-50%)}.hover-content p{color:#000;margin:2% 0 0;text-decoration:none}";
-export {
-  Supervisor_vue_vue_type_style_index_0_lang as default
-};
-//# sourceMappingURL=_id_-styles-2.mjs-e95a85bb.js.map
